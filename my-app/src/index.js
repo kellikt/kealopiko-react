6e5254bfa884d2d75730d2321216013eb64cd56f
kellikt <44486938+kellikt@users.noreply.github.com>
2019-10-14 09:27:47 -1000

    Task 3

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
+import 'semantic-ui-css/semantic.min.css';
+import { Container, Header } from 'semantic-ui-react';
 
 class Kealopiko extends React.Component {
 
   render() {
     return (
-        <h1>Kealopiko!</h1>
-  );
+        <Container textAlign="center">
+          <Header as='h1'>Kealopiko!</Header>
+        </Container>
+    );
   }
 }
 
-ReactDOM.render(<Kealopiko />, document.getElementById('root'));
+ReactDOM.render(<Kealopiko/>, document.getElementById('root'));
